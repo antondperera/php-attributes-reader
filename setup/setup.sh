86a52b2cdd1f74d360185589034ea544f5737efd
Anton Perera <antondilshanperera@gmail.com>
2023-10-01 21:07:28 +0530

    old setup folder removed.

--- setup/setup.sh
+++
@@ -1,8 +0,0 @@
-apt install docker.io -y
-
-iptables -P FORWARD ACCEPT
-
-docker build --no-cache -t localhost:25000/php-attributes-reader:v1.0 -f ./docker/Dockerfile ../
-docker push localhost:25000/php-attributes-reader:v1.0
-
-docker-compose -f ./docker/docker-compose.yml up -d
