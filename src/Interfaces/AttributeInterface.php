f80ffdea47fda2d8239bb76712f1577640da0b95
Anton Perera <antondilshanperera@gmail.com>
2023-10-19 23:13:23 +0530

    feat: Introduce the AttributeInterface.
    Refs: #11

---
+++ src/Interfaces/AttributeInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AntonDPerera\PHPAttributesReader\Interfaces;
+
+interface AttributeInterface
+{
+    public function getClass(): string;
+    public function getName(): string;
+    public function getArguments(): array;
+}
