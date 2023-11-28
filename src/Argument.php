2de4ff10cd0dcb51d7b256791e78a89ea5ecac26
Anton Perera <antondilshanperera@gmail.com>
2023-11-28 22:06:18 +0530

    feat: Argument class introduced.

---
+++ src/Argument.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AntonDPerera\PHPAttributesReader;
+
+class Argument
+{
+    public const ARGUMENT_VALUE_TYPE_EMPTY = 0;
+    public const ARGUMENT_VALUE_TYPE_BOOLEAN = 1;
+    public const ARGUMENT_VALUE_TYPE_BASIC = 2;
+    public const ARGUMENT_VALUE_TYPE_INDEXED_ARRAY = 3;
+    public const ARGUMENT_VALUE_TYPE_ASSOCIATIVE_ARRAY = 4;
+    public const ARGUMENT_VALUE_TYPE_OBJECT = 5;
+
+    public const ARGUMENT_VALUE_TYPE_OTHER = 50;
+
+
+    private mixed $value = null;
+    private mixed $type = null;
+
+    public function __construct(mixed $argument)
+    {
+        $this->type = $this->determineType($argument);
+        $this->value = $argument;
+    }
+
+    private function determineType(mixed $argument): int
+    {
+        if(empty($argument)) {
+            return self::ARGUMENT_VALUE_TYPE_EMPTY;
+        }
+
+        return self::ARGUMENT_VALUE_TYPE_OTHER;
+    }
+
+    public function getValue(): mixed
+    {
+        return $this->value;
+    }
+
+    public function getType(): int
+    {
+        return $this->type;
+    }
+}
