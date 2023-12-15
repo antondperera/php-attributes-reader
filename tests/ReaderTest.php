0662b011fa0e2356c34567888cc33842fd406cb2
Anton Perera <antondilshanperera@gmail.com>
2023-12-15 06:08:40 +0530

    chore: tests function names improvements

--- tests/ReaderTest.php
+++ tests/ReaderTest.php
@@ -11,7 +11,7 @@ use AntonDPerera\PHPAttributesReader\Exceptions\ClassNotFoundException;
 
 class ReaderTest extends TestCase
 {
-    public static function invalidClassNameProvider(): array
+    public static function invalidClassValueProvider(): array
     {
         return [
             [''],
@@ -22,15 +22,15 @@ class ReaderTest extends TestCase
     }
 
     /**
-     * @dataProvider invalidClassNameProvider
+     * @dataProvider invalidClassValueProvider
      */
-    public function testExceptionWhenInvalidClassGiven(?string $class = null): void
+    public function testExceptionWhenInvalidClassValueGiven(?string $class = null): void
     {
         $this->expectException(InvalidClassException::class);
         $reader = new Reader($class);
     }
 
-    public static function nonExistingClassNameProvider(): array
+    public static function nonExistingClassProvider(): array
     {
         return [
             ["notExistingClass"],
@@ -41,7 +41,7 @@ class ReaderTest extends TestCase
     }
 
     /**
-     * @dataProvider nonExistingClassNameProvider
+     * @dataProvider nonExistingClassProvider
      */
     public function testExceptionWhenNonExistingClassGiven(?string $class = null): void
     {
