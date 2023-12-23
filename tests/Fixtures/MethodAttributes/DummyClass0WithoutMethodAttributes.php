6aa5f820c9fcfe20f2e7d6d07e6e5ff2cca3e7bd
Anton Perera <antondilshanperera@gmail.com>
2023-12-24 02:00:31 +0530

    feat: Fixtures added for method attributes

---
+++ tests/Fixtures/MethodAttributes/DummyClass0WithoutMethodAttributes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AntonDPerera\PHPAttributesReader\Tests\Fixtures\MethodAttributes;
+
+class DummyClass0WithoutMethodAttributes
+{
+    public function getDummyMethod1WithoutAttributes(): bool
+    {
+        return true;
+    }
+}
---
+++ tests/Fixtures/MethodAttributes/DummyClass1WithMethodAttributes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AntonDPerera\PHPAttributesReader\Tests\Fixtures\MethodAttributes;
+
+class DummyClass1WithMethodAttributes
+{
+
+    #[TestAttribute]
+    #[TestAttribute2("abc")]
+    public function getDummyMethod2WithAttributes(): bool
+    {
+        return true;
+    }
+}
