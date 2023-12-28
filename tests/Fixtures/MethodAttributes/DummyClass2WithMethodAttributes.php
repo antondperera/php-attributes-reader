2c855afc13a8f0ef104802ee03dc8370d8dbd3af
Anton Perera <antondilshanperera@gmail.com>
2023-12-28 10:46:38 +0530

    fix: Fixed a php warning from a fixture class

--- tests/Fixtures/MethodAttributes/DummyClass2WithMethodAttributes.php
+++ tests/Fixtures/MethodAttributes/DummyClass2WithMethodAttributes.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace AntonDPerera\PHPAttributesReader\Tests\Fixtures\MethodAttributes;
 
-define("DUMMY_ARGUMENT_OBJECT", (object)["property1"=>"property1 value"]);
-
 class DummyClass2WithMethodAttributes
 {
-    #[TestAttribute3(DUMMY_ARGUMENT_OBJECT)]
+    #[TestAttribute3('dummyValue')]
     public function getDummyMethod3WithAttributes(): bool
     {
         return true;
