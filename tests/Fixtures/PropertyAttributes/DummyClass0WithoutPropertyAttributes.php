849804333fcd622c68ada6df144d07754070034e
Anton Perera <antondilshanperera@gmail.com>
2023-12-29 23:40:50 +0530

    feat: Necessary Fixtures for Property Attributes tests added

---
+++ tests/Fixtures/PropertyAttributes/DummyClass0WithoutPropertyAttributes.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AntonDPerera\PHPAttributesReader\Tests\Fixtures\PropertyAttributes;
+
+class DummyClass0WithoutPropertyAttributes
+{
+    public $property1_without_attributes = null;
+}
